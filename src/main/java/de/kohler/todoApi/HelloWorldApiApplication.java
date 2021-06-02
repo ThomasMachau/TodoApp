3bac24f2c4e6cf32ba6484336ae0ed36931b5edc
Thomas <Th.Machau@gmail.com>
2021-06-02 22:01:40 +0200

    init

---
+++ src/main/java/de/kohler/todoApi/HelloWorldApiApplication.java
@@ -0,0 +1,13 @@
+package de.kohler.todoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApiApplication.class, args);
+	}
+
+}
